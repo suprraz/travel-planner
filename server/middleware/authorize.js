87866128e7d10a2f86bb4499abb41621ca6ef2e3
Razvan Suprovici <razvan.suprovici@gmail.com>
2017-02-09 15:30:58 -0800

    authorization roles

--- server/middleware/authorize.js
+++ server/middleware/authorize.js
@@ -4,7 +4,7 @@ module.exports = function authorize(app){
    * Refuses access to everyone except the "admin" user
    */
   function adminOnly(req, res, next) {
-    if (req.user.username === 'admin') {
+    if (req.user.role === 'admin') {
       // you're an admin, so you may proceed
       next();
     }
@@ -14,15 +14,34 @@ module.exports = function authorize(app){
     }
   }
 
+  function managerOrAdminOnly(req, res, next) {
+    if (req.user.role === 'admin' || req.user.role === 'manager' ) {
+      // you're an admin, so you may proceed
+      next();
+    }
+    else {
+      // you're NOT an admin, so denied
+      res.status(401).send('Only managers and admins can access this');
+    }
+  }
 
-  /**
-   * Allows users to perform operations on their own account, but not other people's accounts.
-   * Except for the Admin user, who is allowed to do anything to any account.
-   */
+  function yourselfOrManagerOnly(req, res, next) {
+    var userBeingEdited = req.swagger.params.username;
+
+    if (req.user.username === userBeingEdited || req.user.role === 'admin' || req.user.role === 'manager' ) {
+      // You're editing yourself.  Or you're an admin.  Either way, you may proceed.
+      next();
+    }
+    else {
+      // Nope.  You can't edit other people.
+      res.status(401).send('You can only do this for your own account.');
+    }
+  }
+
   function yourselfOnly(req, res, next) {
     var userBeingEdited = req.swagger.params.username;
 
-    if (req.user.username === userBeingEdited || req.user.username === 'admin') {
+    if (req.user.username === userBeingEdited || req.user.role === 'admin' ) {
       // You're editing yourself.  Or you're an admin.  Either way, you may proceed.
       next();
     }
@@ -33,21 +52,38 @@ module.exports = function authorize(app){
   }
 
 
+  // Only admins/managers are allowed to get the full list of users
+  app.get('/users', managerOrAdminOnly);
+
+  // Only admins/managers can create new users
+  app.post('/users', managerOrAdminOnly);
+
+  // Users/managers can only retrieve their own account
+  app.get('/users/{username}', yourselfOrManagerOnly);
+
+  // Users/managers can only edit their own account
+  app.post('/users/{username}', yourselfOrManagerOnly);
+
+  // Users/managers can only delete their own account
+  app.delete('/users/{username}', yourselfOrManagerOnly);
+
+  // Users/managers can only log themselves out
+  app.post('/users/{username}/logout', yourselfOrManagerOnly);
+
+
   // Only admins are allowed to get the full list of users
-  app.get('/users', adminOnly);
+  app.get('/trips', adminOnly);
 
   // Only admins can create new users
-  app.post('/users', adminOnly);
+  app.post('/trips', yourselfOnly);
 
   // Users can only retrieve their own account
-  app.get('/users/{username}', yourselfOnly);
+  app.get('/trips/{tripId}', yourselfOnly);
 
   // Users can only edit their own account
-  app.post('/users/{username}', yourselfOnly);
+  app.post('/trips/{tripId}', yourselfOnly);
 
   // Users can only delete their own account
-  app.delete('/users/{username}', yourselfOnly);
+  app.delete('/trips/{tripId}', yourselfOnly);
 
-  // Users can only log themselves out
-  app.post('/users/{username}/logout', yourselfOnly);
-}
+};
